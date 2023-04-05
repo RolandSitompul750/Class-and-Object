4cacd5af57a0387c47a06a7e7bb7798f15f0cb3f
RolandSitompul750 <rolandsitompul750@gmail.comm>
2023-04-05 23:02:39 +0700

    upload project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasClassDanObject_TAXI_;
+
+namespace TugasPemrogramanTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "jono";
+            taxi.Onduty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject_TAXI_
+{
+    class Taxi
+    {
+        private readonly bool OnDuty;
+
+        public string DriverName { get; set; }
+        public bool Onduty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : no");
+            }
+            Console.WriteLine("NumPassenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+
+}
